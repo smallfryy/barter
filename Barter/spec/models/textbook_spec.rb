94b4c05bc92a35e738b09e6e09190361792c6c23
smallfryy <holly.m.peck@gmail.com>
2016-04-13 18:35:26 -0400

    start to fix rspec issues

--- Barter/spec/models/textbook_spec.rb
+++ Barter/spec/models/textbook_spec.rb
@@ -14,24 +14,23 @@
 require_relative "../spec_helper.rb"
 
 RSpec.describe Textbook, type: :model do
-  DatabaseCleaner.clean
-
-  textbook = Textbook.create(title: "Math 101", isbn: Faker::Code.isbn)
-  sammy = User.create(first_name: "Sammy", last_name: "Mernick", email: "smernick3@gmail.com", password: "funfunfun")
-  justin = User.create(first_name: "Justin", last_name: "Curhan", email: "justin.curhan@gmail.com", password: "funfunfun")
-  liz = User.create(first_name: "Liz", last_name: "Kalina", email: "liz.kalina@gmail.com", password: "funfunfun")
-  holly = User.create(first_name: "Holly", last_name: "Peck", email: "holly.m.peck@gmail.com", password: "funfunfun")
-  book = UserBook.create(textbook: textbook, user: sammy)
-  book1 = UserBook.create(textbook: textbook, user: sammy)
-  book2 = UserBook.create(textbook: textbook, user: liz)
-  subject1 = Subject.create(name: "Math")
-  subject2 = Subject.create(name: "English")
-  subject3 = Subject.create(name: "Science")
-  text_sub1 = TextbookSubject.create(textbook: textbook, subject: subject1)
-  text_sub2 = TextbookSubject.create(textbook: textbook, subject: subject2)
-
-
+  # DatabaseCleaner.clean
   describe 'relationship' do
+  let!(:textbook){Textbook.create(title: "Math 101", isbn: Faker::Code.isbn)}
+  let!(:sammy){User.create(first_name: "Sammy", last_name: "Mernick", email: "smernick3@gmail.com", password: "funfunfun")}
+  let!(:justin){User.create(first_name: "Justin", last_name: "Curhan", email: "justin.curhan@gmail.com", password: "funfunfun")}
+  let!(:liz){User.create(first_name: "Liz", last_name: "Kalina", email: "liz.kalina@gmail.com", password: "funfunfun")}
+  let!(:holly){User.create(first_name: "Holly", last_name: "Peck", email: "holly.m.peck@gmail.com", password: "funfunfun")}
+  let!(:book){UserBook.create!(textbook: textbook, user: sammy)}
+  let!(:book1){UserBook.create!(textbook: textbook, user: sammy)}
+  let!(:book2){UserBook.create!(textbook: textbook, user: liz)}
+  let!(:subject1){Subject.create(name: "Math")}
+  let!(:subject2){Subject.create(name: "English")}
+  let!(:subject3){Subject.create(name: "Science")}
+  let!(:text_sub1){TextbookSubject.create(textbook: textbook, subject: subject1)}
+  let!(:text_sub2){TextbookSubject.create(textbook: textbook, subject: subject2)}
+
+
     it 'Textbook has a title' do
       expect(textbook.title).to eq("Math 101")
     end
@@ -43,6 +42,7 @@ RSpec.describe Textbook, type: :model do
 
     it "has multilpe users, through user_books" do
       #  textbook.users will return all instances of users, even if a user is selling mutliple coppies of hte same book. that user will be counted for each time they sell book.
+    
       expect(textbook.users.uniq.count).to eq(2)
     end
 
