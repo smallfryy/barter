7fb15a064f3ea9aeec1b314f2d94bc7f07e7ed0c
smernick <smernick3@gmail.com>
2016-04-14 09:42:42 -0400

    fix spec tests

---
+++ Barter/spec/models/karma_spec.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: karmas
+#
+#  id         :integer          not null, primary key
+#  balance    :integer          default(0)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+
+
+require_relative "../spec_helper.rb"
+
+
+RSpec.describe Karma, type: :model do
+
+  user = User.create(first_name: "Holly", last_name: "Peck", email: "holly.m.peck@gmail.com", password: "funfunfun")
+  karma = Karma.create(user: user, balance: Faker::Number.between(0, 500))
+
+
+
+  describe 'relationship' do
+    it 'belongs to a user' do
+      expect(karma.user.email).to eq("holly.m.peck@gmail.com")
+    end
+  end
+
+
+
+end
