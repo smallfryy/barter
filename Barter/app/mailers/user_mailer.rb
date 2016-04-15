2648481ef5a3e6356efddd1a203300821b39f8c7
smallfryy <holly.m.peck@gmail.com>
2016-04-15 12:56:16 -0400

    implemented basic styling for textbook index

--- Barter/app/mailers/user_mailer.rb
+++ Barter/app/mailers/user_mailer.rb
@@ -2,8 +2,18 @@ class UserMailer < ApplicationMailer
 
   def add_user_book(user)
     @user = user
+    @textbook = @textbook.user_book
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Thanks for adding a textbook')
+    mail(to: @user.email, subject: 'Listing Details - #{@textbook.title}')
   end
+
+
+  def order_confirmation(user, cart)
+    @user = user
+    @cart = cart
+    mail(to: @user.email, subject: "Order Placed - Bibliotech")
+  end 
+
+
 
 end
