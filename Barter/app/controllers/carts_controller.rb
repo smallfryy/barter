c96d976ff18e20e0f6bf1890e1af46984a21d014
smallfryy <holly.m.peck@gmail.com>
2016-04-19 09:18:30 -0400

    code cleanup

--- Barter/app/controllers/carts_controller.rb
+++ Barter/app/controllers/carts_controller.rb
@@ -35,8 +35,8 @@ class CartsController < ApplicationController
 
   private
 
-    def set_cart
-      @cart = Cart.find(params[:id])
-    end
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
 
 end
